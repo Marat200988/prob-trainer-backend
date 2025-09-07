e2834549da7581d4b3422ec9bde0d32891fc9858
Marat200988 <mmarat03@gmail.com>
2025-09-07 18:19:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 
 import express from "express";
 import cors from "cors";
-import morgan from "morgan";
 
 // ---------- ENV ----------
 const PORT = Number(process.env.PORT || 10000);
@@ -19,7 +18,6 @@ if (!DEEPSEEK_API_KEY) {
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
-app.use(morgan("dev"));
 
 // ---------- IN-MEMORY STORE ----------
 /**
